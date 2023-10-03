9e73617800adca1afd38d44ddd5afca192ee2d1d
vivi-toto <fernandavitoria856@gmail.com>
2023-10-03 19:23:25 -0400

    adicionado estrutura de descisao

--- desafio2.js
+++ desafio2.js
@@ -1,4 +1,13 @@
-function saldoDeRankeadas(vitorias, derrotas){
+saldoDeRankeadas(180, 23)
+
+function saldoDeRankeadas(vitorias, derrotas) {
     let saldo = vitorias - derrotas;
-    
+    console.log("saldo: " + saldo)
+    if (vitorias <= 10) {
+        let nivel = "Ferro";
+    } else {
+        let nivel = "Bronze";
+    }
+
+    console.log("O Herói tem de saldo de" + vitorias + "vitorias e está no nível de " + nivel)
 }
