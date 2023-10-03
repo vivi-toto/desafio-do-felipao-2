eb3a91d5500310248e085ca76cdf0796fa229478
vivi-toto <fernandavitoria856@gmail.com>
2023-10-03 19:47:00 -0400

    incluido retorno da decisao

--- desafio2.js
+++ desafio2.js
@@ -8,15 +8,21 @@ function saldoDeRankeadas(vitorias, derrotas) {
         console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
     } else if (vitorias >= 11 && vitorias <= 20 ) {
         nivel = "Bronze"
+        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
     } else if (vitorias >= 21 && vitorias <= 50 ) {
         nivel = "Prata"
+        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
     } else if (vitorias >= 51 && vitorias <= 80 ) {
         nivel = "Ouro"
+        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
     } else if (vitorias >= 81 && vitorias <= 90 ) {
         nivel = "Diamante"
+        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
     } else if (vitorias >= 91 && vitorias <= 100 ) {
         nivel = "Lendário"
+        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
     } else {
         nivel = "Imortal"
+        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
     }
 }
