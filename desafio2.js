fa0ae847e0ab638484e6d86fabc79885aa3cbca2
vivi-toto <fernandavitoria856@gmail.com>
2023-10-03 19:43:50 -0400

    alteracao na estrutura de decisao

--- desafio2.js
+++ desafio2.js
@@ -2,12 +2,21 @@ saldoDeRankeadas(180, 23)
 
 function saldoDeRankeadas(vitorias, derrotas) {
     let saldo = vitorias - derrotas;
-    console.log("saldo: " + saldo)
-    if (vitorias <= 10) {
-        let nivel = "Ferro";
+    let nivel;
+    if (vitorias <= 10){
+        nivel = "Ferro";
+        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+    } else if (vitorias >= 11 && vitorias <= 20 ) {
+        nivel = "Bronze"
+    } else if (vitorias >= 21 && vitorias <= 50 ) {
+        nivel = "Prata"
+    } else if (vitorias >= 51 && vitorias <= 80 ) {
+        nivel = "Ouro"
+    } else if (vitorias >= 81 && vitorias <= 90 ) {
+        nivel = "Diamante"
+    } else if (vitorias >= 91 && vitorias <= 100 ) {
+        nivel = "Lendário"
     } else {
-        let nivel = "Bronze";
+        nivel = "Imortal"
     }
-
-    console.log("O Herói tem de saldo de" + vitorias + "vitorias e está no nível de " + nivel)
 }
