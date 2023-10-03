898e46df9c5e428985bf47f953f0990a1da0c938
vivi-toto <fernandavitoria856@gmail.com>
2023-10-03 19:52:37 -0400

    nova saida adicionada

--- desafio2.js
+++ desafio2.js
@@ -5,24 +5,31 @@ function saldoDeRankeadas(vitorias, derrotas) {
     let nivel;
     if (vitorias <= 10){
         nivel = "Ferro";
-        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+        console.log("O herói tem saldo de: " + vitorias + " e está no nível de: " + nivel)
+        console.log("Saldo de rankeada: " + saldo)
     } else if (vitorias >= 11 && vitorias <= 20 ) {
         nivel = "Bronze"
-        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+        console.log("O herói tem saldo de: " + vitorias + " e está no nível de: " + nivel)
+        console.log("Saldo de rankeada: " + saldo)
     } else if (vitorias >= 21 && vitorias <= 50 ) {
         nivel = "Prata"
-        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+        console.log("O herói tem saldo de: " + vitorias + " e está no nível de: " + nivel)
+        console.log("Saldo de rankeada: " + saldo)
     } else if (vitorias >= 51 && vitorias <= 80 ) {
         nivel = "Ouro"
-        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+        console.log("O herói tem saldo de: " + vitorias + " e está no nível de: " + nivel)
+        console.log("Saldo de rankeada: " + saldo)
     } else if (vitorias >= 81 && vitorias <= 90 ) {
         nivel = "Diamante"
-        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+        console.log("O herói tem saldo de: " + vitorias + " e está no nível de: " + nivel)
+        console.log("Saldo de rankeada: " + saldo)
     } else if (vitorias >= 91 && vitorias <= 100 ) {
         nivel = "Lendário"
-        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+        console.log("O herói tem saldo de: " + vitorias + " e está no nível de: " + nivel)
+        console.log("Saldo de rankeada: " + saldo)
     } else {
         nivel = "Imortal"
-        console.log("O herói tem saldo de: " + vitorias + "e está no nível de: " + nivel)
+        console.log("O herói tem saldo de: " + vitorias + " e está no nível de: " + nivel)
+        console.log("Saldo de rankeada: " + saldo)
     }
 }
