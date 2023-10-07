8df5874b071ae71f21f3942eb28ca0f7ddd48c24
vivi-toto <fernandavitoria856@gmail.com>
2023-10-06 23:06:02 -0400

    inicializada a variavel quantidade de golpes

--- desafioloop.js
+++ desafioloop.js
@@ -1,13 +1,10 @@
-const quantidadeGolpes = parseInt(gets());
+const quantidadeGolpes = 4;
 
-// TODO: Defina aqui os tipos de minerais Carvao, Ferro, Diamante e Pedra
 let minerais = ["Carvao", "Ferro", "Diamante", "Pedra"];
 
-// Loop para cada golpe, de 1 até a quantidade informada
 for (let i = 1; i <= quantidadeGolpes; i++) {
-  // Calcula o índice do mineral usando o operador de módulo (%) para garantir que o índice esteja dentro do tamanho do array
+
   let minaIndex = i % minerais.length;
   
-  // TODO: Agora exiba o índice i, concatene com o caractere ":", após, concatene com tipo de minerais[minaIndex]:
-  print(    );
+  print(i + ":" + minerais[minaIndex]);
 }
