9c0fbcf66e0f3bec8c59bde5c9752ad19f450e23
vivi-toto <fernandavitoria856@gmail.com>
2023-10-06 23:17:59 -0400

    atualização

--- desafioloop.js
+++ desafioloop.js
@@ -6,5 +6,5 @@ for (let i = 1; i <= quantidadeGolpes; i++) {
 
   let minaIndex = i % minerais.length;
   
-  print(i + ":" + minerais[minaIndex]);
+  console.log(i + ":" + minerais[minaIndex - 1]);
 }
