5078104ddc14497d893afb370d00a12ea1525ea8
Fernanda Vitória <75960737+vivi-toto@users.noreply.github.com>
2023-10-11 21:18:23 -0400

    Update desafioloop.js

--- desafioloop.js
+++ desafioloop.js
@@ -8,3 +8,9 @@ for (let i = 0; i < quantidadeGolpes; i++) {
   
   console.log((i + 1) + ": " + minerais[minaIndex]);
 }
+
+// com esses dados de entrada, a saída deverá ser a seguinte: 
+// 1: carvao
+// 2: Ferro
+// 3: Diamante
+// 4: Pedra
